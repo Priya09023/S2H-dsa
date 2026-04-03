c916e1966fa7a3f382ff1a99d441a1c7088cc8d6
Priya09023 <priyabandaru090@gmail.com>
2026-04-03 09:30:33 +0530

    Day 2: Reverse String problem

---
+++ Day-02-Strings/01-Reverse-String/solution.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+for _ in range(t):
+    s = input()
+    s = list(s)
+
+    i, j = 0, len(s) - 1
+
+    while i < j:
+        s[i], s[j] = s[j], s[i]
+        i += 1
+        j -= 1
+
+    print("".join(s))
